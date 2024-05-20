cff0311a47a239ead7ef616951173ab82449cd83
PhamDangHai <haipham20002000@gmail.com>
2024-05-20 10:40:34 +0700

    create form project system-management-service

---
+++ src/main/java/mcnex/com/mes/systemmanagement/MesSystemManagementApplication.java
@@ -0,0 +1,13 @@
+package mcnex.com.mes.systemmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesSystemManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesSystemManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mcnex/com/mes/systemmanagement/MesSystemManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package mcnex.com.mes.systemmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesSystemManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
